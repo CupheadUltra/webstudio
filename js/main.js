34c880f7e017ae0bb530aa27f6b1ffb7d17e4447
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2024-01-16 18:53:25 +0200

    modal anf footer form

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // Фільтр проектів в порфтоліо
-const list = document.querySelector(".port-list");
-const items = document.querySelectorAll(".port-item");
+const list = document.querySelector('.port-list');
+const items = document.querySelectorAll('.port-item');
 
 function filter() {
-  const filters = document.querySelectorAll(".filters-item");
+  const filters = document.querySelectorAll('.filters-item');
 
-  filters.forEach((filter) => {
-    filter.addEventListener("click", (event) => {
+  filters.forEach(filter => {
+    filter.addEventListener('click', event => {
       const targetId = event.currentTarget.dataset.id;
       console.log(targetId);
 
-      items.forEach((item) => {
-        if (targetId === "all" || item.classList.contains(targetId)) {
-          item.style.display = "block";
+      items.forEach(item => {
+        if (targetId === 'all' || item.classList.contains(targetId)) {
+          item.style.display = 'block';
         } else {
-          item.style.display = "none";
+          item.style.display = 'none';
         }
       });
     });
@@ -22,3 +22,19 @@ function filter() {
 }
 
 filter();
+
+// Модельне вікно для кнопки "Замовити послугу"
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
